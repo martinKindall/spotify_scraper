2c8e05b2bc9e6baa12c5a3d708da7df2738d1337
martinKindall <mart256@gmail.com>
2021-08-08 22:20:07 +0200

    Added data type for parsing Songs

--- SpotifyScrapper.py
+++ SpotifyScrapper.py
@@ -1,3 +1,5 @@
+from typing import Iterator
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -5,16 +7,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from dataTypes.Song import Song
+
 baseURL = 'https://spotifycharts.com/regional/'
 
 
 class SpotifyScrapper:
 
-    def requestAndObtainTopSongs(self):
+    def requestAndObtainTopSongs(self) -> Iterator[Song]:
         driver = webdriver.Chrome(
             executable_path=r'C:\\webdrivers\\chromedriver.exe'
         )
-        driver.get(baseURL + 'co/daily/2021-08-04')
+        country = 'co'
+        date = '2021-08-04'
+        driver.get(baseURL + '{}/daily/{}'.format(country, date))
         delay = 20
         try:
             flyTabs = WebDriverWait(driver, delay).until(
@@ -22,11 +28,24 @@ class SpotifyScrapper:
             generalDetails: BeautifulSoup = BeautifulSoup(driver.page_source, "html.parser")
             songsList = generalDetails.find('table', {'class': 'chart-table'}) \
                             .find_all('tr')[1:]
+            return map(
+                lambda songRaw: self.parseSong(songRaw, country, date),
+                songsList
+            )
 
         except TimeoutException:
             print("Loading took too much time!")
             exit()
 
+    def parseSong(self, songRaw: BeautifulSoup, country: str, date: str) -> Song:
+        nameAndArtist = songRaw.find('td', {'class': 'chart-table-track'})
+        name = nameAndArtist.find('strong').getText()
+        artist = nameAndArtist.find('span').getText()
+        position = songRaw.find('td', {'class': 'chart-table-position'}).getText()
+        return Song(int(position), name, artist, date, country)
+
 
 if __name__ == '__main__':
-    SpotifyScrapper().requestAndObtainTopSongs()
+    songs = list(SpotifyScrapper().requestAndObtainTopSongs())
+    import pdb; pdb.set_trace()
+    print("hi")
---
+++ dataTypes/Song.py
@@ -0,0 +1,13 @@
+
+class Song:
+    def __init__(self,
+                 ranking: int,
+                 name: str,
+                 author: str,
+                 date: str,
+                 country: str):
+        self.ranking = ranking
+        self.name = name
+        self.author = author
+        self.date = date
+        self.country = country
